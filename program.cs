15b3b0ff4c07a04bfa2c0a9060b9f91a0bcbe526
EMCET HAZANİ <140171298+EMCET-HAZANi@users.noreply.github.com>
2023-07-29 00:18:59 +0300

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Emt Mağazamızı Hoş Geldiniz ^_^ ");
+            double urun_fiyati, kargo, toplam;
+            Console.Write("Lütfen Ürün Fiyatını Giriniz : ");
+            urun_fiyati = double.Parse(Console.ReadLine());
+
+            if (urun_fiyati >= 100)
+
+            {
+                Console.WriteLine("Kargonuz Ücretsiz Olacaktır.");
+            }
+
+            else if (urun_fiyati < 100)
+
+            {
+                kargo  = 5;
+                toplam = urun_fiyati + kargo;
+                Console.WriteLine("Kargoya  5'Tl Fazla Alınacaktır " + ":" + toplam );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+        
